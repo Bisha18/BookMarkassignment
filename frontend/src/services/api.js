5f19b6e88087c28885e0223eab86df41c57c975c
bishal <d.bishalpaul@gmail.com>
2026-02-15 13:25:14 +0530

    added updated url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,10 @@
 // src/services/api.js
 import axios from "axios";
 
+const url = import.meta.env.VITE_API_URL;
+
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: url,
   headers: { "Content-Type": "application/json" },
   timeout: 10_000,
 });
